7e49f9f5f7ad371db7772328bdc9aeb9e1d8d5ea
Sarah Marshall <sr70079@gmail.com>
2021-01-17 17:25:10 -0500

    current weather call

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,55 +2,74 @@ $(document).ready(function() {
 
     // This is our API key
     var APIKey = "a789e74543ab2eb315de8d5bcab5a0b9";
+    var lon = 0.0;
+    var lat = 0.0;
 
-    var cities = [];
-
-    // Here we are building the URL we need to query the database
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-      "q=" + cities + "&appid=" + APIKey;
-
-    // Here we run our AJAX call to the OpenWeatherMap API
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      // We store all of the retrieved data inside of an object called "response"
-      .then(function(response) {
-
-        // Log the queryURL
-        console.log(queryURL);
-
-        // Log the resulting object
-        console.log(response);
-
-        // Transfer content to HTML
-        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-        $(".wind").text("Wind Speed: " + response.wind.speed);
-        $(".humidity").text("Humidity: " + response.main.humidity);
-        // $(."uv").text("UV Index: " + response.) need to find uv index
+    var createLi = function(response) {
+        var ul = $("<ul>");
+        var cityLi = $("<li>").text(response.name)
+        var windLi = $("<li>").text("Wind Speed: " + response.wind.speed);
+        var humidityLi = $("<li>").text("Humidity: " + response.main.humidity);
+        // var uvLi = $("<li>").text("UV Index: " + response.) need to find uv index
         
-        // Convert the temp to fahrenheit
         var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        var tempLi = $("<li>").text("Temperature (F) " + tempF.toFixed(2));
 
-        // add temp content to html
-        $(".temp").text("Temperature (K) " + response.main.temp);
-        $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+        ul.append(cityLi, windLi, humidityLi, tempLi)
 
-        // Log the data in the console as well
-        console.log("Wind Speed: " + response.wind.speed);
-        console.log("Humidity: " + response.main.humidity);
-        console.log("Temperature (F): " + tempF);
-      });
+        $("#main").append(ul);
+    };
 
+    var searchCity = function(city) {
+        // Here we are building the URL we need to query the database
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-      $("#search").on("click", function() {
+        // Here we run our AJAX call to the OpenWeatherMap API
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        // We store all of the retrieved data inside of an object called "response"
+        .then(function(response) {
+            // Log the resulting object
+            console.log(response);
 
-            var city = $("#city-search").val().trim();
+            createLi(response);
 
-            cities.push(city);
+            
+            // Transfer content to HTML
+            // $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+            // $(".wind").text("Wind Speed: " + response.wind.speed);
+            // $(".humidity").text("Humidity: " + response.main.humidity);
+            //$(."uv").text("UV Index: " + response.) need to find uv index
+            
+            // Convert the temp to fahrenheit
+            var tempF = (response.main.temp - 273.15) * 1.80 + 32;
 
-            response();
+            // add temp content to html
+            // $(".temp").text("Temperature (K) " + response.main.temp);
+            // $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
 
-      });
+            // Log the data in the console as well
+            console.log("Wind Speed: " + response.wind.speed);
+            console.log("Humidity: " + response.main.humidity);
+            console.log("Temperature (F): " + tempF);
 
+            var oneCallQueryUrl = "https://api.openweathermap.org/data/2.5/onecall?lon=" + response.lon + "&lat=" + response.lat + "&appid=" + APIKey;
+
+            // Here we run our AJAX call to the OpenWeatherMap API
+            $.ajax({
+                url: oneCallQueryUrl,
+                method: "GET"
+            })
+            // We store all of the retrieved data inside of an object called "response"
+            .then(function(response) {
+                console.log(response);
+            });
+        });
+    };
+
+    $("#search").on("click", function() {
+        searchCity($("#city-search").val().trim());
+    });
 });
